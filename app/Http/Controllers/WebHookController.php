535912fdd13a581f23b8720596d4103d1785a2cb
tri setianto <tri.setianto@outlook.com>
2017-09-05 03:49:44 +0800

    initial commit

---
+++ app/Http/Controllers/WebHookController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tri
+ * Date: 05/09/17
+ * Time: 2:13
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use phpseclib\Net\SSH2;
+
+class WebHookController extends Controller
+{
+    public function passtelWeb(Request $request)
+    {
+        // Log::info(var_dump($request, true));
+        Log::info(print_r($request->input('repository'),true));
+        //Log::info(var_dump($request->input('push')));
+
+        $ssh = new SSH2('10.11.5.130', '1922');
+
+        if (!$ssh->login('admeen', 'admeen123')) {
+            return response('gagal connect', 500);
+        }
+        $ssh->write("cd /var/www/web/passtel-web\n");
+        $ssh->read('$');
+        $ssh->write("git pull\n");
+        $ssh->read(".org':");
+        $ssh->write("Timeline10\n");
+        $hasil = $ssh->read('$');
+
+        return $hasil;
+    }
+}
